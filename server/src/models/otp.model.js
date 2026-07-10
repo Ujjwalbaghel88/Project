b08ee1bc6ec6a8995034cc123b5fd101c6f58b39
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-07-10 20:01:43 +0530

    Otp Model In Backend

---
+++ server/src/models/otp.model.js
@@ -0,0 +1,16 @@
+const otpSchema = new mongoose.Schema(
+{
+    
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    otp:{
+        type:String,
+        required:true,
+    },
+    expires
+}
+
+)
